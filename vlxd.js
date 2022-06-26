84536ad115677a3a25bea2ba57602c158a945640
ishopjapan <thoitran2107@gmail.com>
2022-06-26 13:38:22 +0700

    html

---
+++ vlxd.js
@@ -0,0 +1,22 @@
+let listProduct = [];
+class Product {
+    name
+    price
+
+    constructor(name, price) {
+        this.name=name;
+        this.price=price;
+    }
+    getName(name){
+        document.getElementById("name1").value;
+    }
+    getPrice(price){
+        document.getElementById("number1").value;
+    }
+
+}
+let catvang = new Product("cát vàng","100.000");
+document.getElementById("cát vàng");
+document.getElementById("100.000");
+console.log(catvang)
+
